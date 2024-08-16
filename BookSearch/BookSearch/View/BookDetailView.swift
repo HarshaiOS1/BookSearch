7b5f0449fcf95a76cf74081f2d8120e1ed0b57ec
HarshaiOS1 <46565732+HarshaiOS1@users.noreply.github.com>
2024-08-16 16:52:57 +0200

    updated documentation and cleaned code

--- BookSearch/BookSearch/View/BookDetailView.swift
+++ BookSearch/BookSearch/View/BookDetailView.swift
@@ -7,14 +7,22 @@
 
 import SwiftUI
 
+/// `BookDetailView` presents detailed information about a specific book.
+///
+/// This view displays the book's thumbnail, title, authors, and description. It also includes a favorite button to toggle the book's favorite status.
+///
+/// - Note: The `book` property is a `BookEntity` that represents the book to be displayed.
+/// The `viewModel` property is used to interact with the underlying data model for updating the book's favorite status.
 struct BookDetailView: View {
+    /// The view model that provides data and functionality related to books.
     @ObservedObject var viewModel: BooksViewModel
+    /// The book entity whose details are to be displayed.
     @State var book: BookEntity
     
     var body: some View {
         HStack {
             if let url = book.thumbnail {
-                CustomImageView(url: URL(string: url), placeholder: Image.init(systemName: "photo.on.rectangle.angled"))
+                CustomImageView(url: URL(string: url),placeholder: Image.init(systemName: "photo.on.rectangle.angled"))
                     .frame(maxWidth: 100)
             }
             VStack {
@@ -41,6 +49,10 @@ struct BookDetailView: View {
         .navigationTitle("Book Details")
     }
     
+    /// Toggles the favorite status of the book and updates the view model.
+    ///
+    /// This function changes the `isFavorite` property of the book and calls the `updateBookFavoriteStatus` method on the view model
+    /// to persist the change.
     private func toggleFavorite() {
         book.isFavorite.toggle()
         viewModel.updateBookFavoriteStatus(book)
